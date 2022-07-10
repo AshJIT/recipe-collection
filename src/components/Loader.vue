a768cd620be1229431c59cbbac46e9efa34962c3
Ash Julian <32071682+AshJIT@users.noreply.github.com>
2022-07-10 13:09:04 -0300

    Basic search

---
+++ src/components/Loader.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="loader__container">
+        <div class="loader"></div>
+        <span>Loading, please wait...</span>
+    </div>
+</template>
+
+<style lang="scss">
+    .loader {
+        border: 16px solid #f3f3f3;
+        border-top: 16px solid #3498db;
+        border-radius: 50%;
+        width: 60px;
+        height: 60px;
+        animation: spin 2s linear infinite;
+
+        &__container {
+            position: fixed;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+        }
+    }
+
+    @keyframes spin {
+        0% { transform: rotate(0deg); }
+        100% { transform: rotate(360deg); }
+    }
+</style>
